e66a313ff3d22ab6a9540a5cb934b923b8ed3794
Roy Mosby <10133316+royemosby@users.noreply.github.com>
2025-01-18 16:34:17 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pleasant Start
+
+React project template undergirded by Vite and the vanilla react template. Sprinkled with opinions and assets for a quick start.
+
+- uses [Reasonable Colors](https://www.reasonable.work/colors/) for color custom properties
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import templateLogo from './assets/images/pleasant-start.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main>
+        <img src={templateLogo} className="logo" alt="React logo" />
+      </main>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
